648cc718c8f6c8bed07d391bd7a2262ff7de9df5
tumdelonge <tumdelonge@gmail.com>
2022-10-20 23:43:16 +0700

    python_loop

---
+++ for_loop.py
@@ -0,0 +1,21 @@
+# looping
+# while loop
+# for loop
+# nested loop
+
+# 2. for_loop
+# for i in range(10):
+#     print("cetak ke-",i)
+
+# # coba2
+# for i in range(1, 10):
+#     print("cetak ke-",i)
+
+# # coba3
+# for i in range(2, 11, 2):
+#     print("angka ke-",i)
+
+
+# coba4
+for i in range(1, 11, 2):
+    print("angka ke-",i)
---
+++ while_loop.py
@@ -0,0 +1,47 @@
+
+# looping
+# while loop
+# for loop
+# nested loop
+
+# 1. while loop
+# num = 7
+# c = 1
+
+# while c <= num:
+#     print("print-",c)
+#     c += 1
+
+# # coba2
+# print("Cetak angka ASC")
+# c = int(input("Masukkan angka awal : "))
+# num = int(input("Masukkan angka akhir : "))
+# while c <= num: 
+#     print("angka ke-", c)
+#     c += 1
+
+# # coba3
+# print("Cetak angka DESC")
+# c = int(input("Masukkan angka awal : "))
+# num = int(input("masukkan angka akhir : "))
+# while c >= num:
+#     print("cetak angka ke-",c)
+#     c -= 1
+
+# # cona4
+# print("Cetak bilangan genap")
+# c = int(input("Masukkan angka awal : "))
+# num = int(input("Masukkan angka maksimal : "))
+# while c <= num:
+#     if c % 2 == 0 :
+#         print(c)
+#     c += 1
+
+# coba5
+print("Cetak bilangan ganjil")
+c = int(input("Masukkan angka awal : "))
+num = int(input("Masukkan angka akhir : "))
+while c <= num:
+    if c % 2 == 1:
+        print(c)
+    c += 1
